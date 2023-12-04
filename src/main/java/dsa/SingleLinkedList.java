8dc88b2255394a3e21dd9adf37c33044d7057402
Abirami Palanikumar <abiramipalanikumar179@gmail.com>
2023-12-04 18:56:50 +0530

    Added Base List

---
+++ src/main/java/dsa/SingleLinkedList.java
@@ -0,0 +1,55 @@
+package dsa;
+
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int data)
+    {
+        this.data = data;
+        this.next = null;
+    }
+
+    public void setNext(Node node)
+    {
+        this.next = node;
+    }
+}
+
+public class SingleLinkedList
+{
+
+    Node head;
+
+    SingleLinkedList()
+    {
+        head = null;
+    }
+
+    public void push_back(int data) {
+
+    }
+
+    public void push_front(int data) {
+
+    }
+
+    public int pop_back() {
+        return 0;
+    }
+
+    public int pop_front() {
+        return 0;
+    }
+
+    public int length()
+    {
+        return 0;
+    }
+
+    public boolean isEmpty()
+    {
+       return head == null;
+    }
+}
---
+++ src/test/java/dsa/SingleLinkedListTest.java
@@ -0,0 +1,39 @@
+package dsa;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SingleLinkedListTest 
+{
+
+    @Test
+    public void testEmptyList()
+    {
+        SingleLinkedList list = new SingleLinkedList();
+        assertEquals(true, list.isEmpty());
+    }
+
+    @Test
+    public void testListSize() {
+
+    }   
+    
+    @Test
+    public void testListPush_back() {}
+
+    @Test 
+    public void testListPush_front() {
+
+    }
+
+    @Test 
+    public void testListPop_back() {
+
+    }
+
+    @Test
+    public void testListPop_front() {
+        
+    }
+}
