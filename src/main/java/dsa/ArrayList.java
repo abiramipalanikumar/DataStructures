48da5674209df987f460ccd88449b0090f71ebbd
Abirami Palanikumar <abiramipalanikumar179@gmail.com>
2023-12-04 17:43:16 +0530

    UnitTesting

--- src/main/java/dsa/ArrayList.java
+++ src/main/java/dsa/ArrayList.java
@@ -1,15 +1,49 @@
 package dsa;
 
 public class ArrayList {
-    int value;
     int[] list;
-    int index = 0, size = 50;
+    int index, size;
+
+    public ArrayList(int size)
+    {
+        this.size = size;
+        this.index = 0;
+        this.list = new int[size];
+    }
 
     public ArrayList()
-    {}
+    {
+        this(50);
+    }
 
     public void add(int value)
-    {}
+    {
+        if(index == size)
+        {
+            this.size = this.size + 50;
+            int[] newList = new int[size];
+            for(int i = 0;i < index; i++)
+            {
+                newList[i] = list[i];
+            }
+            this.list = newList;
+        }
+        list[index++] = value;
+    }
+
+    public int get(int index) throws ArrayIndexOutOfBoundsException
+    {
+        if(index < 0 || index >= this.index)
+        {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        return list[index];
+    }
+
+    public int length()
+    {
+        return this.index;
+    }
 
     public int size()
     {
--- src/test/java/dsa/ArrayListTest.java
+++ src/test/java/dsa/ArrayListTest.java
@@ -3,10 +3,67 @@ package dsa;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
+// import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
 public class ArrayListTest {
     @Test
     public void shouldReturnArrayDefualtSize() {
         ArrayList array = new ArrayList();
-        assertEquals(50, array.size());
+        assertEquals(50, array.size());        
+    }
+
+    @Test
+    public void insertArrayValues()
+    {
+        ArrayList array = new ArrayList();
+        for(int i = 0; i < 100; i++)
+        {
+            array.add(i);
+        }
+
+        for(int i = 0; i < 100; i++)
+        {
+            assertEquals(i, array.get(i));
+        }
+
+        assertEquals(100, array.size());
+    }
+
+
+    @Test
+    public void checkArraySize()
+    {
+        ArrayList array = new ArrayList();
+        for(int i = 0; i < 120; i++)
+        {
+            array.add(i);
+        }
+
+        for(int i = 0; i < 120; i++)
+        {
+            assertEquals(i, array.get(i));
+        }
+
+        assertEquals(150, array.size());
+        assertEquals(120, array.length());
+    }
+
+    @Test
+    public void checkArrayIndexOutOfBoundary()
+    {
+        ArrayList array = new ArrayList();
+        for(int i = 0; i < 120; i++)
+        {
+            array.add(i);
+        }
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> {
+            array.get(125);
+        });
+
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> {
+            array.get(-1);
+        });
     }
 }
